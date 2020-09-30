d0f2a3be10ef2a72b4fb93b5ff7efc28ece2e635
rayyh <rayyounghong@gmail.com>
2020-09-30 11:58:17 +0800

    tiny update

--- src/core/control_flows/while_statement.py
+++ src/core/control_flows/while_statement.py
@@ -2,8 +2,9 @@ def sum_from_1_to_n(n):
     num_sum = 0
     i = 1
     while i <= n:
-        num_sum = num_sum + i
-        i = i + 1
+        # The ++ operator is not available in Python.
+        num_sum += i
+        i += 1
     return num_sum
 
 
@@ -11,6 +12,6 @@ def while_loop_with_else():
     counter = 0
     while counter < 3:
         print("Inside loop", end=" ")
-        counter = counter + 1
+        counter += 1
     else:
         print("Inside else", end=" ")
