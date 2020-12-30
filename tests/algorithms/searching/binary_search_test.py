3d2847547ad2cd1ccb3f33f6ca9981b6b28a75f4
rayyh <rayyounghong@gmail.com>
2020-12-30 22:27:55 +0800

    use short names

--- tests/algorithms/searching/binary_search_test.py
+++ tests/algorithms/searching/binary_search_test.py
@@ -2,7 +2,7 @@ from src.algorithms.sorting.helper import is_sorted
 from src.algorithms.searching.binary_search import binary_search
 
 
-def test_binary_search():
+def test_search():
     sorted_list = [1, 3, 5, 7, 9]
     assert is_sorted(sorted_list)
     assert binary_search(sorted_list, 3) == 1
--- tests/algorithms/searching/linear_search_test.py
+++ tests/algorithms/searching/linear_search_test.py
@@ -1,7 +1,7 @@
 from src.algorithms.searching.linear_search import linear_search
 
 
-def test_binary_search():
+def test_search():
     collection = [1, 3, 5, 11, 32, 98, 7, 9]
     assert linear_search(collection, 3) == 1
     assert linear_search(collection, 9) == len(collection) - 1
