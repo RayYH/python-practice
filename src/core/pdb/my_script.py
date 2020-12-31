175ea632018efdbdfb16c3cb10a4b553db37c8c9
rayyh <rayyounghong@gmail.com>
2020-12-31 17:01:08 +0800

    fix different case

--- src/core/pdb/my_script.py
+++ src/core/pdb/my_script.py
@@ -1,11 +1,11 @@
 # https://book.pythontips.com/en/latest/debugging.html
 # $ python -m pdb my_script.py
 # c: continue execution
-# w: shows the context of the current line it is executing.
+# w: shows the context of the current line it is executing
 # a: print the argument list of the current function
-# s: Execute the current line and stop at the first possible occasion.
-# n: Continue execution until the next line in the current function is reached
-#    or it returns.
+# s: execute the current line and stop at the first possible occasion
+# n: continue execution until the next line in the current function is reached
+#    or it returns
 import pdb
 
 
