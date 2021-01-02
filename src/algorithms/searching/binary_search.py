86ed446d8eced7aafbb484630943a2cf08f947b9
rayyh <rayyounghong@gmail.com>
2021-01-02 14:25:11 +0800

    add recursive_binary_search (ita 2.3-5)

--- src/algorithms/searching/binary_search.py
+++ src/algorithms/searching/binary_search.py
@@ -1,3 +1,18 @@
+def recursive_binary_search(sorted_list, target):
+    # base case
+    if len(sorted_list) == 0:
+        return None
+
+    # divide conquer
+    mid = len(sorted_list) // 2
+    if sorted_list[mid] == target:
+        return mid
+    elif sorted_list[mid] < target:
+        return mid + recursive_binary_search(sorted_list[mid:], target)
+    else:
+        return recursive_binary_search(sorted_list[:mid], target)
+
+
 def binary_search(sorted_list, target):
     low = 0
     high = len(sorted_list) - 1
--- tests/algorithms/searching/binary_search_test.py
+++ tests/algorithms/searching/binary_search_test.py
@@ -1,9 +1,17 @@
 from src.algorithms.sorting.helper import is_sorted
-from src.algorithms.searching.binary_search import binary_search
+from src.algorithms.searching.binary_search import binary_search, \
+    recursive_binary_search
 
 
-def test_search():
+def test_binary_search():
     sorted_list = [1, 3, 5, 7, 9]
     assert is_sorted(sorted_list)
     assert binary_search(sorted_list, 3) == 1
     assert not binary_search(sorted_list, -1)
+
+
+def test_recursive_binary_search():
+    sorted_list = [1, 3, 5, 7, 9]
+    assert is_sorted(sorted_list)
+    assert recursive_binary_search(sorted_list, 3) == 1
+    assert not recursive_binary_search(sorted_list, -1)
