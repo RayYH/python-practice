23ed7150030c17003e837f0e96719d234503a065
RayYH <RayYH@users.noreply.github.com>
2021-01-05 13:19:47 +0000

    Apply yapf changes

--- tests/algorithms/arrays/powers_of_sorted_array.py
+++ tests/algorithms/arrays/powers_of_sorted_array.py
@@ -6,31 +6,24 @@ def test_solution(case):
     assert solution(case['nums']) == case['ans']
 
 
-@pytest.fixture(params=[
-    {
-        'nums': [],
-        'ans': 0
-    },
-    {
-        'nums': [-3, -2, -1, 0],
-        'ans': 4
-    },
-    {
-        'nums': [-3, -2, -2, -2, -1, 0],
-        'ans': 4
-    },
-    {
-        'nums': [-1, -1, -1, 1, 1, 1, 1],
-        'ans': 1
-    },
-    {
-        'nums': [-3, -3, -3, -3],
-        'ans': 1
-    },
-    {
-        'nums': [-3, -2, 1, 2, 3, 4, 9, 9, 9, 11],
-        'ans': 6
-    }
-])
+@pytest.fixture(params=[{
+    'nums': [],
+    'ans': 0
+}, {
+    'nums': [-3, -2, -1, 0],
+    'ans': 4
+}, {
+    'nums': [-3, -2, -2, -2, -1, 0],
+    'ans': 4
+}, {
+    'nums': [-1, -1, -1, 1, 1, 1, 1],
+    'ans': 1
+}, {
+    'nums': [-3, -3, -3, -3],
+    'ans': 1
+}, {
+    'nums': [-3, -2, 1, 2, 3, 4, 9, 9, 9, 11],
+    'ans': 6
+}])
 def case(request):
     yield request.param
