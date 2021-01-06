d3975ac1aeada2e642e7c3fe6b5f24f06b4fe8fc
rayyh <rayyounghong@gmail.com>
2021-01-06 17:22:23 +0800

    Remove useless comments tests

--- tests/core/comments/basic_test.py
+++
@@ -1,7 +0,0 @@
-def test_comments():
-    # this is the first comment
-    spam = 1  # and this is the second comment
-    # ... and now a third!
-    text = "# This is not a comment because it's inside quotes."
-    assert spam
-    assert text
