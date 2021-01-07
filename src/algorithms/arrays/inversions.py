fcbf098dd546111247d2a4c632a32b95b7973ef6
rayyh <rayyounghong@gmail.com>
2021-01-07 13:59:48 +0800

    add explaination for inversions algorithm

--- src/algorithms/arrays/inversions.py
+++ src/algorithms/arrays/inversions.py
@@ -1,13 +1,30 @@
 def merge_inversions(arr, p, q, r):
+    """
+    Merge sorted array arr[p:q] and arr[q:r] such that arr[p:r] is sorted
+    """
     left, right = arr[p:q].copy(), arr[q:r].copy()
     left_len, right_len = len(left), len(right)
-    i = j = inversions = 0
+    # i - current index of left part
+    # j - current index of right part
+    i = j = 0
+    inversions_count = 0
     k = p
+    # whether we have counted the merge-inversions involving right[j]
+    # think about two arrays: [4, 5, 6] (left) and [1, 2, 3] (right)
+    # we compare the first element 4 in left array and the first
+    # element 1 in right array, since 4 (left) > 1 (right), all
+    # elements in the left array after 4 should be counted
+    # because 6 > 5 > 4 > 1.
     counted = False
     while i < left_len and j < right_len:
+        # count inversions first, then move right pointer
         if not counted and right[j] < left[i]:
-            inversions += left_len - i
+            inversions_count += left_len - i
             counted = True
+        # If left[i] <= right[j], there's no need to move i and no new
+        # inversions occur, just skip, but if left[i] > right[j], new
+        # inversions occur, we should count previous inversions of
+        # right[j-1] and reset the counted flag.
         if left[i] <= right[j]:
             arr[k] = left[i]
             i += 1
@@ -15,25 +32,32 @@ def merge_inversions(arr, p, q, r):
             arr[k] = right[j]
             j += 1
             counted = False
+            # At here, inversions of arr[j] has already been counted!
+            # we just move right pointer and reset the counted flag
         k += 1
     remain_elements = left[i:] + right[j:]
     arr[k:r] = remain_elements
 
-    return inversions
+    return inversions_count
 
 
 def count_inversions(arr, p, r):
-    # wht not p < r? since, we use q instead of q+1
-    inversions = 0
+    """
+    Count the inversions of arr[p:r].
+    """
+    inversions_count = 0
+    # At least two elements in arr[p:r]
     if p < r - 1:
         q = (p + r) // 2
-        inversions += count_inversions(arr, p, q)  # arr[p:q] is sorted
-        inversions += count_inversions(arr, q, r)  # arr[q:r] is sorted
-        inversions += merge_inversions(arr, p, q, r)
-    return inversions
+        inversions_count += count_inversions(arr, p, q)
+        inversions_count += count_inversions(arr, q, r)
+        inversions_count += merge_inversions(arr, p, q, r)
+    # p >= r - 1, think about arr[r-1,r] which contains 1 element
+    # So at here, at most one elements in arr returns 0 directly
+    return inversions_count
 
 
-def number_of_inversions(arr):
+def inversions(arr):
     """
     Let A[1..n] be an array of n distinct numbers.
     If i < j and A[i] > A[j], then the pair (i, j) is called an inversion of A.
--- tests/algorithms/arrays/inversions_test.py
+++ tests/algorithms/arrays/inversions_test.py
@@ -1,9 +1,12 @@
-from src.algorithms.arrays.inversions import number_of_inversions
+from src.algorithms.arrays.inversions import inversions
 
 
 def test_inversions():
-    assert number_of_inversions([]) == 0
-    assert number_of_inversions([1, 2, 3, 4, 5, 6]) == 0
-    assert number_of_inversions([2, 3, 8, 6, 1]) == 5
-    assert number_of_inversions([4, 5, 6, 1, 2, 3]) == 9
-    assert number_of_inversions([4, 5, 6, 1, 2, 3, 0]) == 15
+    assert inversions([]) == 0
+    assert inversions([1, 2, 3, 4, 5, 6]) == 0
+    assert inversions([2, 3, 8, 6, 1]) == 5
+    assert inversions([4, 5, 6, 1, 2, 3]) == 9
+    assert inversions([4, 5, 6, 1, 2, 3, 0]) == 15
+    assert inversions([3, 2, 1]) == 3
+    assert inversions([2, 2, 2, 2, 2, 2, 2, 2]) == 0
+    assert inversions([4, 5, 6, 1, 10, 11]) == 3
