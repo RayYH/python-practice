4dd30767975d329695e048e5b02213dba7d63e77
rayyh <rayyounghong@gmail.com>
2021-01-11 17:34:07 +0800

    remove confused test case

--- tests/core/oop/oop_random_remarks_test.py
+++ tests/core/oop/oop_random_remarks_test.py
@@ -11,26 +11,3 @@ def test_attribute_lookup_prioritizes():
     w2.region = 'east'
     assert w2.purpose == 'storage'
     assert w2.region == 'east'
-
-
-def min_f(self, x, y):
-    assert self
-    return min(x, y)
-
-
-class C:
-    f = min_f
-
-    def g(self):
-        assert self
-        return 'hello'
-
-    h = g
-
-
-def test_c():
-    c = C()
-    # noinspection PyArgumentList
-    assert c.f(1, 2) == 1
-    assert c.h() == 'hello'
-    assert C.h(c) == 'hello'
