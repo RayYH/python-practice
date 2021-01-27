adb90a435070058cdf1c7eca3df246fe2213a39e
RayYH <RayYH@users.noreply.github.com>
2021-01-27 13:00:25 +0000

    Apply yapf changes

--- src/algorithms/arrays/groups_of_special_equivalent_strings.py
+++ src/algorithms/arrays/groups_of_special_equivalent_strings.py
@@ -5,7 +5,6 @@ def num_of_special_equivalent_strings(collection: List[str]) -> int:
     """
     https://leetcode.com/problems/groups-of-special-equivalent-strings/
     """
-
     def count(word):
         ans = [0] * 52
         for j, letter in enumerate(word):
