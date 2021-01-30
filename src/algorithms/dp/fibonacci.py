8b4806508fa89743f17bb8c06c8672eeeef90da2
rayyh <rayyounghong@gmail.com>
2021-01-30 21:02:57 +0800

    Fix memoization

--- src/algorithms/dp/fibonacci.py
+++ src/algorithms/dp/fibonacci.py
@@ -8,17 +8,16 @@ def memoization(n: int) -> int:
         if n <= 1:
             MEMO[n] = n
         else:
-            MEMO[n] = MEMO[n - 1] + MEMO[n - 2]
+            MEMO[n] = memoization(n - 1) + memoization(n - 2)
 
     return MEMO[n]
 
 
 def tabulation(n: int) -> int:
-    tables = [-1] * (n + 1)
-
     if n <= 1:
         return n
 
+    tables = [-1] * (n + 1)
     tables[0:2] = [0, 1]
 
     for i in range(2, n + 1):
